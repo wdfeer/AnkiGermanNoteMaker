62f8c09e7cab0384c4df8dbbd697ad438b998da8
wdfeer <wdf-is-git@tutanota.de>
2022-07-10 16:40:58 +0200

    Use addNoteTemplate.json as an EmbeddedResource

--- AnkiConnectLib/Notes.cs
+++ AnkiConnectLib/Notes.cs
@@ -1,19 +1,29 @@
 ﻿using System.Net;
+using System.Reflection;
 
 namespace AnkiConnectLib;
 public static class Notes
 {
     public static string AddNote(string deckName, string modelName, string front, string back)
     {
-        string data = File.ReadAllText(@"C:\Users\wdf-win\source\repos\AnkiConnectLib\addNoteTemplate.json");
+        var assembly = Assembly.GetExecutingAssembly();
+        string data;
+        using (Stream stream = assembly.GetManifestResourceStream("AnkiConnectLib.addNoteTemplate.json")!)
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            data = reader.ReadToEnd();
+        }
+
         data = data.Replace("$deckName", deckName)
                    .Replace("$modelName", modelName)
                    .Replace("$front", front)
                    .Replace("$back", back);
 
-        WebClient client = new WebClient();
-        client.Headers[HttpRequestHeader.ContentType] = "application/json";
-        string response = client.UploadString(Main.URI, data);
-        return response;
+        using (WebClient client = new WebClient())
+        {
+            client.Headers[HttpRequestHeader.ContentType] = "application/json";
+            string response = client.UploadString(Main.URI, data);
+            return response;
+        }
     }
 }
